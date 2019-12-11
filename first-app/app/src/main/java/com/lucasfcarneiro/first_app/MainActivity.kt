a00645d0c206cd6723e2bdb80bf8969d7d4fe556
lucas <lukasfcarneiro@gmail.com>
2019-12-10 23:03:36 -0300

    Change text method created

--- first-app/app/src/main/java/com/lucasfcarneiro/first_app/MainActivity.kt
+++ first-app/app/src/main/java/com/lucasfcarneiro/first_app/MainActivity.kt
@@ -2,11 +2,23 @@ package com.lucasfcarneiro.first_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        changeText()
     }
+
+    public fun changeText() {
+
+
+        butaoButton.setOnClickListener{
+            clickBelowTextView.text = "Paralelepipedo"
+        }
+    }
+
+
 }
