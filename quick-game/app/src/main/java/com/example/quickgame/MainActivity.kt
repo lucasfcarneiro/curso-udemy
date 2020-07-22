a31b576b33cbadd74348bfedbfdd5e25da26f29d
Lucas Fagundes <lukasfcarneiro@gmail.com>
2020-07-22 19:49:26 -0300

    Quick game layout finished

---
+++ quick-game/app/src/main/java/com/example/quickgame/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.quickgame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        paperHandImageView.setOnClickListener{
+            resultText.setText("asdasd")
+        }
+
+        fun Selecionado()
+        {
+
+        }
+
+
+    }
+
+
+}
