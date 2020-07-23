afc571405c8c13e97e132a4670975c16679cd21d
Lucas Fagundes <lukasfcarneiro@gmail.com>
2020-07-23 13:54:23 -0300

    Quick game programming started

--- quick-game/app/src/main/java/com/example/quickgame/MainActivity.kt
+++ quick-game/app/src/main/java/com/example/quickgame/MainActivity.kt
@@ -2,7 +2,11 @@ package com.example.quickgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.random.Random.Default.nextInt
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,17 +14,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        paperHandImageView.setOnClickListener{
-            resultText.setText("asdasd")
+        rockHandImageView.setOnClickListener{chosenRock()}
+        paperHandImageView.setOnClickListener{chosenPaper()}
+        scissorHandImageView.setOnClickListener {chosenScissor()}
         }
 
-        fun Selecionado()
-        {
 
-        }
+    fun optionChosen (){
+
+    }
+    private fun chosenRock (){
+        resultText.text = "escolhido pedra"
+
+    }
+    private fun chosenPaper(){
+        resultText.text = "escolhido papel"
+    }
+    private fun chosenScissor(){
+        resultText.text = "escolhido tesoura"
+    }
+
+    fun IntRange.random() =
+        Random().nextInt((endInclusive + 1) - start) + start
+
+
+
+
+
 
 
     }
 
 
-}
+
