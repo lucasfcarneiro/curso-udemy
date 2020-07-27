65bdcfc5ca5cd29ab579dce83f79ec049ca810c8
lucas <lukasfcarneiro@gmail.com>
2020-07-27 17:23:10 -0300

    Basic components Input created

--- app/src/main/java/com/example/basic_components/MainActivity.kt
+++ app/src/main/java/com/example/basic_components/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.basic_components
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        oioi
+
+        sendButton.setOnClickListener{enviar()}
     }
+    fun enviar (){
+
+    }
+
 }
