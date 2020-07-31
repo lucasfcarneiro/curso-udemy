a525c657e3ae4c421bdfd5495cadc12f2ddb7714
Lucas Fagundes <lukasfcarneiro@gmail.com>
2020-07-31 19:05:08 -0300

    Beer value updated

--- beer-values/app/src/main/java/com/example/beer_values/MainActivity.kt
+++ beer-values/app/src/main/java/com/example/beer_values/MainActivity.kt
@@ -8,52 +8,53 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        /*Ml250Button.setOnClickListener { calc250(250) }
-        Ml350Button.setOnClickListener { calc350(350) }
-        Ml473Button.setOnClickListener { calc473(473) }
-        Ml600Button.setOnClickListener { calc600(600) }
 
-         */
         calcButton.setOnClickListener { calcular()}
+
     }
 
     private fun calcular (){
         if (Ml250Radio.isChecked){
-            calc250(250)
+            calc(250)
         }
         if (Ml350Radio.isChecked){
-            calc350(350)
+            calc(350)
         }
         if (Ml473Radio.isChecked){
-            calc473(473)
+            calc(473)
         }
         if (Ml600Radio.isChecked){
-            calc600(600)
+            calc(600)
+        }
+
+
+    }
+
+
+    private fun calc(x:Int){
+        val qtdlata = 1000 / x
+        val preçoLitro = qtdlata * firstPriceEditText.text.toString().toDouble()
+        resultFirstTextView.text = "O preço do litro é: $preçoLitro reais"
+        if (Ml250Radio2.isChecked){
+            calc2(250)
+        }
+        if (Ml350Radio2.isChecked){
+            calc2(350)
+        }
+        if (Ml473Radio2.isChecked){
+            calc2(473)
+        }
+        if (Ml600Radio2.isChecked){
+            calc2(600)
         }
     }
-
-
-    private fun calc250(x:Int){
+    private fun calc2(x:Int){
         val qtdlata = 1000 / x
-        val preçolitro = unitValueTextView.text.toString().toDouble() * qtdlata
-        resultFirstBeerTextView.text = "Preço por litro: $preçolitro"
-    }
-    private fun calc350(x:Int){
-        val qtdlata = 1000 / x
-        val preçoLitro = unitValueTextView.text.toString().toDouble() * qtdlata
-        resultFirstBeerTextView.text = "Preço por litro: $preçoLitro"
-    }
-    private fun calc473(x:Int){
-        val qtdlata = 1000 / x
-        val preçoLitro = unitValueTextView.text.toString().toDouble() * qtdlata
-        resultFirstBeerTextView.text = "Preço por litro: $preçoLitro"
-    }
-    private fun calc600(x:Int){
-        val qtdlata = 1000 / x
-        val preçoLitro = unitValueTextView.text.toString().toDouble() * qtdlata
-        resultFirstBeerTextView.text = "Preço por litro: $preçoLitro"
+        val preçoLitro = qtdlata * firstPriceEditText.text.toString().toDouble()
+        resultSecondTextView.text = "O preço do litro é: $preçoLitro reais"
 
     }
+
 
 
 }
