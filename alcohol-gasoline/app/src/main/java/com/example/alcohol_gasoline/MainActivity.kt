a3d87ea467fc2b59d9e44f566c20e4f619887741
Lucas Fagundes <lukasfcarneiro@gmail.com>
2020-07-31 01:50:34 -0300

    Alcohol gasoline updated

--- alcohol-gasoline/app/src/main/java/com/example/alcohol_gasoline/MainActivity.kt
+++ alcohol-gasoline/app/src/main/java/com/example/alcohol_gasoline/MainActivity.kt
@@ -2,12 +2,32 @@ package com.example.alcohol_gasoline
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        calcButton.setOnClickListener { getPrices() }
+
+
+
     }
 
+    private fun getPrices(){
+        val alcoholPrice = alcoolEditText.text.toString().toDouble()
+        val gasolinePrice = gasolinaEditText.text.toString().toDouble()
 
+        var divisao = gasolinePrice / alcoholPrice
+        if(divisao <= 0.7){
+            resultTextView.text = "Melhor o alcool"
+        }else {
+            resultTextView.text = "Melhor a gasolina"
+        }
+        limpar()
+    }
+    private fun limpar ( ){
+        alcoolEditText.cleaTextView
+    }
 }
