4f4f61cf1502ec0a269386369ead6b2e4e9da22e
Lucas Fagundes <lukasfcarneiro@gmail.com>
2020-08-04 22:07:48 -0300

    Alcohol gasoline refactored

--- alcohol-gasoline/app/src/main/java/com/example/alcohol_gasoline/MainActivity.kt
+++ alcohol-gasoline/app/src/main/java/com/example/alcohol_gasoline/MainActivity.kt
@@ -3,36 +3,49 @@ package com.example.alcohol_gasoline
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         calcButton.setOnClickListener { getPrices() }
+        cleaTextView.setOnClickListener { cleanFields() }
     }
 
     private fun getPrices(){
-        val alcoholPrice = alcoolEditText.text.toString().toDouble()
-        val gasolinePrice = gasolinaEditText.text.toString().toDouble()
+        val alcoholPrice = alcoholInputText.text.toString().toDouble()
+        val gasolinePrice = gasolineInputText.text.toString().toDouble()
 
        val fieldValidated = validateFields(alcoholPrice,gasolinePrice)
+
         if(fieldValidated){
             val division = alcoholPrice / gasolinePrice
             if(division <= 0.7){
-                resultTextView.text = getString(R.string.betterAlcohol)
+                resultTextView.text =  getString(R.string.betterAlcohol)
             }
-            else resultTextView.text = getString(R.string.betterGasoline)
+            else {
+                resultTextView.text = getString(R.string.betterGasoline)
+            }
+            percentTextView.text = "$division é o preço do alcool em relação a gasolina"
         }
+
     }
     private fun validateFields (x: Double, y : Double ): Boolean{
         var isValid = true
+
         if(x.equals("")){
             isValid = false
         }else if (y.equals("")){
             isValid = false
         }
         return isValid
+    }
+
+    private fun cleanFields(){
+        alcoholInputText.setText("")
+        alcoholInputText.setText("")
+        resultTextView.text = ""
+        percentTextView.text = ""
     }
 
 }
