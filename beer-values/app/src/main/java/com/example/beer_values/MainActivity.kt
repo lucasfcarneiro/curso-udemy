9fca309b2b2fe4a2282699b82867fb4de8120711
Lucas Fagundes <lukasfcarneiro@gmail.com>
2020-08-05 00:02:20 -0300

    Beer value code refactor started

--- beer-values/app/src/main/java/com/example/beer_values/MainActivity.kt
+++ beer-values/app/src/main/java/com/example/beer_values/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.beer_values
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.roundToInt
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,65 +15,46 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         calcButton.setOnClickListener {
-            var selecao1 = verificarSelecionado1()
-            var selecao2 = verificarSelecionado2()
-            var PLitro1 = calcular(selecao1)
-            var PLitro2 =calcular(selecao2)
-            resultFirstTextView.text = "O preço do litro é: $PLitro1 reais"
-            resultSecondTextView.text = "O preço do litro é: $PLitro2 reais"
-            if(PLitro1 > PLitro2){
-                resultFinalTextView.text="Melhor comprar o $PLitro2"
-            }else resultFinalTextView.text="Melhor comprar o $PLitro1"
+            val firstRadioButtonValue = checkRadioButtonSelection(radioGroup)
+            val secondRadioButtonValue = checkRadioButtonSelection(radioGroup2)
+            val PLitro1 = calcular(firstRadioButtonValue)
+            val PLitro2 = calcular(secondRadioButtonValue)
+            resultFirstTextView.text = "O preço do litro é: ${String.format("%.2f", PLitro1)} reais"
+            resultSecondTextView.text = "O preço do litro é: ${PLitro2 } reais"
+            if (PLitro1 > PLitro2) {
+                resultFinalTextView.text = "Melhor comprar o $PLitro2"
+            } else resultFinalTextView.text = "Melhor comprar o $PLitro1"
         }
 
     } //função
 
 
-        private fun calcular(x: Double): Double {
-            val qtdlata: Double = (1000 / x)
-            val preçoLitro = qtdlata * firstPriceEditText.text.toString().toDouble()
-            return preçoLitro
-        }
+    private fun calcular(x: Double): Double {
+        val qtdlata: Double = (1000 / x)
+        val preçoLitro = qtdlata * firstPriceEditText.text.toString().toDouble()
+        return preçoLitro
+    }
 
-        private fun verificarSelecionado1(): Double {
-            if (Ml250Radio.isChecked) {
-                var preço = 250.0
-                return preço
-            }
-            if (Ml350Radio.isChecked) {
-                var preço = 350.0
-                return preço
-            }
-            if (Ml473Radio.isChecked) {
-                var preço = 473.0
-                return preço
-            }
-            if (Ml600Radio.isChecked) {
-                var preço = 600.0
-                return preço
-            }
-            return 0.0
-        }
+    private fun checkRadioButtonSelection(radioGroup: RadioGroup): Double {
 
-        private fun verificarSelecionado2(): Double {
-            if (Ml250Radio2.isChecked) {
-                var preço = 250.0
-                return preço
+        return when (findViewById<View>(radioGroup.checkedRadioButtonId) as RadioButton) {
+            Ml250Radio, Ml250Radio2 -> {
+                250.0
             }
-            if (Ml350Radio2.isChecked) {
-                var preço = 350.0
-                return preço
+            Ml350Radio, Ml350Radio2 -> {
+                350.0
             }
-            if (Ml473Radio2.isChecked) {
-                var preço = 473.0
-                return preço
+            Ml473Radio, Ml473Radio2 -> {
+                473.0
             }
-            if (Ml600Radio2.isChecked) {
-                var preço = 600.0
-                return preço
+            Ml600Radio, Ml600Radio2 -> {
+                600.0
             }
-            return 0.0
+            else -> {
+                0.0
+            }
         }
     }
+}
 
 
