c3576aa690d4bb96d9d77f8d63fc1bf60f8ecb4d
Lucas Fagundes <lukasfcarneiro@gmail.com>
2021-02-15 19:13:10 -0300

    Recycler view finished

--- recycler-view/app/src/main/java/com/example/recycler_view/MainActivity.kt
+++ recycler-view/app/src/main/java/com/example/recycler_view/MainActivity.kt
@@ -2,23 +2,31 @@ package com.example.recycler_view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.recycler_view.adapter.Movie
+import com.example.recycler_view.adapter.MovieListAdapter
+import com.example.recycler_view.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var movieListAdapter : MovieListAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        //config adapter
-
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         //config recycler view
 
+        movieListAdapter = MovieListAdapter(Movie.DUMBLIST)
+        //binding.movieRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.movieRecyclerView.adapter = movieListAdapter
 
     }//funcao
 
 
 
-
-
 }
+
+
---
+++ recycler-view/app/src/main/java/com/example/recycler_view/adapter/Movie.kt
@@ -0,0 +1,37 @@
+package com.example.recycler_view.adapter
+
+data class Movie(
+    val title: String,
+    val year: String,
+    val genre: String
+) {
+    companion object {
+        val DUMBLIST = listOf(
+            Movie(
+                title = "asdsdasdasddasd",
+                year = "asdasd",
+                genre = "asdasdasd"
+            ),
+            Movie(
+                title = "asdasd",
+                year = "asdasd",
+                genre = "asdasdasd"
+            ),
+            Movie(
+                title = "asdasd",
+                year = "asdasdasddddddddasd",
+                genre = "asdasdasd"
+            ),
+            Movie(
+                title = "asdaaaaaaaaaaaaasd",
+                year = "asdasddaa",
+                genre = "asdasdasd"
+            ),
+            Movie(
+                title = "asddauuuuuasd",
+                year = "asdasd",
+                genre = "asdasdasd"
+            )
+        )
+    }
+}
