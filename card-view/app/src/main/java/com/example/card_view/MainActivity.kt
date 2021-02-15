2f5c661abdaa6d5664c1385e94ba0b0a9b54a39c
Lucas Fagundes <lukasfcarneiro@gmail.com>
2021-02-15 19:58:02 -0300

    Card-view project created

---
+++ card-view/app/src/main/java/com/example/card_view/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.card_view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.card_view.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
