3db9687ff48c4513c496281776b3924d694b79b1
lucas <lukasfcarneiro@gmail.com>
2021-02-26 17:29:05 -0300

    Fragment refactored

--- fragments/app/src/main/java/com/example/fragments/activities/MainActivity.kt
+++ fragments/app/src/main/java/com/example/fragments/activities/MainActivity.kt
@@ -4,16 +4,35 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.fragments.R
 import com.example.fragments.databinding.ActivityMainBinding
+import com.example.fragments.fragments.ChatFragment
+import com.example.fragments.fragments.ContactFragment
 
-private lateinit var binding : ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        
+        val chatFragment = ChatFragment()
+        val contactFragment = ContactFragment()
 
-
+        binding.chatButton.setOnClickListener{
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.mainFrameLayoutFragment, chatFragment)
+                //addToBackStack(null)    **para criar pilha de frag e ao clicar voltar nao fechar o app
+                commit()
+            }
+        }
+        binding.contactButton.setOnClickListener{
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.mainFrameLayoutFragment, contactFragment)
+                commit()
+            }
+        }
 
     }
 
--- fragments/app/src/main/java/com/example/fragments/fragments/ContactFragment.kt
+++ fragments/app/src/main/java/com/example/fragments/fragments/ContactFragment.kt
@@ -17,7 +17,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [ChatFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ChatFragment : Fragment(R.layout.fragment_chat) {
+class ContactFragment : Fragment(R.layout.fragment_contacts) {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
